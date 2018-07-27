a8d88af86f7c1720fdbde74ecdfdf0f68c2f2ed5
Justice Matotoka <justicematotoka@Justices-MacBook-Pro-3.local>
2018-07-27 10:19:20 +0200

    Fixing the port dependancy issue

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ var express = require('express')
   , server = http.createServer(app)
   , io = require('socket.io').listen(server);
 
-server.listen(8080);
+//server.listen(8080);
+
+
+server.listen(process.env.PORT || 8080, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 app.use(express.static('assets'));
 
@@ -49,24 +54,7 @@ io.sockets.on('connection', function (socket) {
         // store the username in the socket session for this client
         socket.emit("We are checking for updates here");
     });
-    //io.to('my room').emit('hello', msg);
 
-	// // when the client emits 'adduser', this listens and executes
-	// socket.on('adduser', function(username){
-	// 	// store the username in the socket session for this client
-	// 	socket.username = username;
-	// 	// store the room name in the socket session for this client
-	// 	socket.room = 'room1';
-	// 	// add the client's username to the global list
-	// 	usernames[username] = username;
-	// 	// send client to room 1
-	// 	socket.join('room1');
-	// 	// echo to client they've connected
-	// 	socket.emit('updatechat', 'SERVER', 'you have connected to room1');
-	// 	// echo to room 1 that a person has connected to their room
-	// 	socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
-	// 	socket.emit('updaterooms', rooms, 'room1');
-	// });
 	
 	// when the client emits 'sendchat', this listens and executes
 	socket.on('sendchat', function (data) {
